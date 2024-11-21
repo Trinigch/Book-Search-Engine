7eb12b11fe3bd6892c496143125ebda19db15af8
Trinigch <trinigch@gmail.com>
2024-11-21 14:55:29 -0600

    fig bug in connection to DB

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,17 +3,8 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
-
-const db = async (): Promise<typeof mongoose.connection> => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log('Database connected.');
-    return mongoose.connection;
-  } catch (error) {
-    console.error('Database connection error:', error);
-    throw new Error('Database connection failed.');
-  }
-};
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks';
+mongoose.connect(MONGODB_URI);
+const db = mongoose.connection
 
 export default db;
